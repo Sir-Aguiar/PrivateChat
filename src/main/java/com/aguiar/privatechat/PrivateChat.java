f19fc3383235a33403aa16fbd6798b0202648069
FELIPE_AGUIAR <felipeferreiraaguiar9@gmail.com>
2024-06-28 17:43:14 -0400

    Initial Commit

---
+++ src/main/java/com/aguiar/privatechat/PrivateChat.java
@@ -0,0 +1,17 @@
+package com.aguiar.privatechat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PrivateChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
